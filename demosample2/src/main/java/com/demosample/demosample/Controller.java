9930274a2748156b40bcff0738d73af98473fd97
trive <trive@192.168.0.118>
2020-02-22 09:57:15 +0530

    fiirst commit basic program

---
+++ demosample2/src/main/java/com/demosample/demosample/Controller.java
@@ -0,0 +1,16 @@
+package com.demosample.demosample;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController  
+	public class Controller   
+	{  
+	@RequestMapping("/")  
+	public String hello()   
+	{  
+	return "Hello javaTpoint";  
+	}  
+	}  
+
+
