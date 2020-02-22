a01d0c0bf4eca3f49651ded510af4ff1722e33f4
keerthi506 <61325911+keerthi506@users.noreply.github.com>
2020-02-22 21:32:11 +0530

    Second commit

--- demosample2/src/main/java/com/demosample/demosample/Controller.java
+++ demosample2/src/main/java/com/demosample/demosample/Controller.java
@@ -1,16 +1,36 @@
 package com.demosample.demosample;
 
+import java.util.List;
+import java.util.ArrayList;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController  
 	public class Controller   
 	{  
-	@RequestMapping("/")  
+	@RequestMapping("msg")  
 	public String hello()   
 	{  
-	return "Hello javaTpoint";  
+	return "Hello World";  
+	}
+	@RequestMapping("name")
+	public String test()
+	{
+		return "keerthi";
+	}
+	
+	@RequestMapping("details")
+     List<Empdetails>  space() {
+		
+	List<Empdetails> list = new ArrayList<Empdetails>();
+	list.add(new Empdetails("keerthi",22));
+	list.add(new Empdetails("karthika",24));
+	 return list;
+	
+	
+	
 	}  
-	}  
+}
+ 
 
 
